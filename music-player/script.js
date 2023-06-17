91c7c57cf383024b2c86e5f23af1ed43f486c434
Angie-540 <janiceangella21@gmail.com>
2023-06-17 05:53:22 +0300

    functionality

--- music-player/script.js
+++ music-player/script.js
@@ -1,47 +1,48 @@
-const musicContainer = document.getElementById('music-container');
-const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+const musicContainer = document.getElementById('music-container')
+const playBtn = document.getElementById('play')
+const  prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+const audio = document.getElementById('audio')
+const progress = document.getElementById('progress')
+const progressContainer = document.getElementById('progress-container')
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
 
-const audio = document.getElementById('audio');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const title = document.getElementById('title');
-const cover = document.getElementById('cover');
-
-const songs = ['hey', 'summer', 'ukulele'];
+//song titles
+const songs = ['hey', 'summer', 'ukulele']
 
 //keep track of songs
-let songIndex = 1;
+let songIndex = 0
 
-//load song info into DOM
-loadSong(songs[songIndex]);
+//Initially load song into DOM
+loadSong(songs[songIndex])
 
-// Update song details
-function loadSong (song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `images/${song}.jpg`;
+//Update song details
+function loadSong(song) {
+    title.innerText = song
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
 }
 
-function playSong() {
-    musicContainer.classList.add('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    playBtn.querySelector('i.fas').classList.add('fa-pause');
+function playSong () {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
 
     audio.play()
+
 }
 
-
-function pauseSong() {
-    musicContainer.classList.remove('play');
-    playBtn.querySelector('i.fas').classList.add('fa-play');
-    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+function pauseSong () {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
 
     audio.pause()
+
 }
 
-function prevSong() {
+function prevSong () {
     songIndex--
 
     if(songIndex < 0) {
@@ -50,52 +51,55 @@ function prevSong() {
 
     loadSong(songs[songIndex])
 
-    playSong
+    playSong()
+
 }
 
-function nextSong() {
+function nextSong () {
     songIndex++
 
-    if(songIndex > songs.length - 1) {
+    if(songIndex > songs.length -1) {
         songIndex = 0
     }
 
     loadSong(songs[songIndex])
 
     playSong()
+    
 }
 
-function updateProgess(e) {
-    const {duration, currentTime } = e.srcElement
+function updateProgress (e) {
+    const {duration, currentTime} = e.srcElement
     const progressPercent = (currentTime / duration) * 100
-    progress.style.width = `${progressPercent}`
+    progress.style.width = `${progressPercent}%`
+
 }
 
 function setProgress(e) {
     const width = this.clientWidth
-    const clickX = e.offsteX
+    const clickX = e.offsetX
     const duration = audio.duration
 
     audio.currentTime = (clickX / width) * duration
-
 }
 
 //Event listeners
-playBtn.addEventListener('click' () => {
+playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
-
-    if(isPlaying) {
-        pauseSong();
+ 
+    if (isPlaying) {
+        pauseSong ()
     } else {
-        playSong();
+        playSong ()
     }
-});
+  
+})
 
-// change song events
+//Change song events
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
 
-audio.addEventListener('timeupdate', updateProgess)
+audio.addEventListener('timeupdate', updateProgress)
 
 progressContainer.addEventListener('click', setProgress)
 
